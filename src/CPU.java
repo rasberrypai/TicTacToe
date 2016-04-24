f0825aaf845784adb1892c6a99afe38d07c37422
Ajay <rasberrypai@gmail.com>
2016-04-23 23:17:15 -0700

    inefficient

--- src/CPU.java
+++ src/CPU.java
@@ -43,6 +43,84 @@ public class CPU {
 									} else if(turn == 1){
 										turn=4;
 									}
+									for(int tii = 0; tii < 3; tii++){
+										for( int ti = 0; ti < 3; ti++){
+											if(tempLoc[ti][tii] == 0){
+												tempLoc[ti][tii] = (byte)turn;
+												canWin();
+												if(turn == 4){
+													turn=1;
+												} else if(turn == 1){
+													turn=4;
+												}
+												for(int fii = 0; fii < 3; fii++){
+													for( int fi = 0; fi < 3; fi++){
+														if(tempLoc[fi][fii] == 0){
+															tempLoc[fi][fii] = (byte)turn;
+															canWin();
+															if(turn == 4){
+																turn=1;
+															} else if(turn == 1){
+																turn=4;
+															}
+															for(int fiveii = 0; fiveii < 3; fiveii++){
+																for( int fivei = 0; fivei < 3; fivei++){
+																	if(tempLoc[fivei][fiveii] == 0){
+																		tempLoc[fivei][fiveii] = (byte)turn;
+																		canWin();
+																		if(turn == 4){
+																			turn=1;
+																		} else if(turn == 1){
+																			turn=4;
+																		}
+																		for(int sxii = 0; sxii < 3; sxii++){
+																			for( int sxi = 0; sxi < 3; sxi++){
+																				if(tempLoc[sxi][sxii] == 0){
+																					tempLoc[sxi][sxii] = (byte)turn;
+																					canWin();
+																					if(turn == 4){
+																						turn=1;
+																					} else if(turn == 1){
+																						turn=4;
+																					}
+																					for(int sii = 0; sii < 3; sii++){
+																						for( int si = 0; si < 3; si++){
+																							if(tempLoc[si][sii] == 0){
+																								tempLoc[si][sii] = (byte)turn;
+																								canWin();
+																								if(turn == 4){
+																									turn=1;
+																								} else if(turn == 1){
+																									turn=4;
+																								}
+																								for(int eii = 0; eii < 3; eii++){
+																									for( int ei = 0; ei < 3; ei++){
+																										if(tempLoc[ei][eii] == 0){
+																											tempLoc[ei][eii] = (byte)turn;
+																											canWin();
+																											if(turn == 4){
+																												turn=1;
+																											} else if(turn == 1){
+																												turn=4;
+																											}
+																										}
+																									}
+																								}
+																							}
+																						}
+																					}
+																				}
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
 								}
 							}
 						}
